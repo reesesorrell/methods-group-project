fa4246af78b9b06d8eceb92e81868b0bad258572
Reese Sorrell <joreesesorrell@gmail.com>
2023-11-09 08:53:16 -0600

    Create view cart function inside of cart

--- cart.py
+++ cart.py
@@ -1,7 +1,9 @@
+import sqlite3
+
 class Cart:
     def __init__(self, databaseName="", inventoryName=""):
-        self.__databaseName = databaseName
-        self.__inventoryName = inventoryName
+        self.databaseName = databaseName
+        self.inventoryName = inventoryName
     
     '''
     Displays all books in the logged in
@@ -9,16 +11,29 @@ class Cart:
     correct information on the inventory items - it just selectively shows the books in the
     User`s cart
     '''
-    def viewCart(userID, inventoryDatabase):
-        pass
+    def viewCart(self, userID, inventoryDatabase):
+        connection = sqlite3.connect(self.databaseName)
+        cursor = connection.cursor()
+        cursor.execute(f'''SELECT i.Title, i.Author, c.Quantity, 
+                        FROM {inventoryDatabase} as i, {self.databaseName} as c 
+                        WHERE c.UserID={userID} AND c.ISBN=i.ISBN''')
+        result = cursor.fetchall()
 
+        print("Your Current Shopping Cart:")
+        print("Title - Author  xQuantity")
+        for listing in result:
+            listingRow = f"{listing[0]} - {listing[1]} x{listing[2]}"
+            print(listingRow)
+
+        cursor.close()
+        connection.close()
 
     '''
     This relies on the user viewing the inventory
     previously - from the main. Once they select a book, this ISBN is used to add an item to
     the appropriate cart
     '''
-    def addToCart(userID, ISBN):
+    def addToCart(self, userID, ISBN):
         pass
 
     
@@ -27,7 +42,7 @@ class Cart:
     previous - from the main. Once they select a book to remove, this ISBN is used to
     remove an item from the user`s cart
     '''
-    def removeFromCart(userID, ISBN):
+    def removeFromCart(self, userID, ISBN):
         pass
 
     '''
@@ -35,5 +50,5 @@ class Cart:
     calls the Inventory class function to decrease the stock of the books by the correct
     amount the user bought (prior to removing them from the cart)
     '''
-    def checkOut(userID):
+    def checkOut(self, userID):
         pass
